aa264e33d45b8f0baa483e11bf5da808950e69ed
TE 01 <viralfinaz@gmail.com>
2022-12-24 14:41:20 +0700

    update

---
+++ src/com/vialfinaz14/Chapter3/Latihan/Listing3.3/SubtractionQuiz.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SubtractionQuiz {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan Nilai Ujian: ");
+        double nilaiUjian = input.nextDouble();
+        char indeksPrestasi;
+
+        if (nilaiUjian >= 90)
+        indeksPrestasi = 'A';
+        else if (nilaiUjian >= 80)
+        indeksPrestasi = 'B';
+        else if (nilaiUjian >= 70)
+        indeksPrestasi= 'C';
+        else if (nilaiUjian >= 60)
+        indeksPrestasi = 'D';
+        else 
+        indeksPrestasi = 'F';
+
+        System.out.println(" Nilai ujian akhir anda adalah " + indeksPrestasi);
+    }
+}
---
+++ src/com/vialfinaz14/Chapter3/Tugas1.java
@@ -0,0 +1,10 @@
+package com.vialfinaz14.Chapter3;
+
+import java.util.Scanner;
+
+public class Tugas1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+}
