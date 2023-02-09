3a0bac9cf37bfbdc810c44a1797957799b52cca0
TE 01 <viralfinaz@gmail.com>
2023-02-09 16:02:18 +0700

    update

--- src/com/vialfinaz14/Chapter7_ArraySingleDimens/Tugas/Exercises15_EliminateDuplicates.java
+++ src/com/vialfinaz14/Chapter7_ArraySingleDimens/Tugas/Exercises15_EliminateDuplicates.java
@@ -1,4 +1,9 @@
 package com.vialfinaz14.Chapter7_ArraySingleDimens.Tugas;
 
+import java.util.Scanner;
+
 public class Exercises15_EliminateDuplicates {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
 }
--- src/com/vialfinaz14/Chapter7_ArraySingleDimens/Tugas/Exercises1_AssignGrades.java
+++ src/com/vialfinaz14/Chapter7_ArraySingleDimens/Tugas/Exercises1_AssignGrades.java
@@ -6,24 +6,40 @@ public class Exercises1_AssignGrades {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number of students: ");
-        int[] schor = new int[input.nextInt()];
-        char[] grade = new char[schor.length];
-        getgrade(new int[]{1, 2});
+        int[] score = new int[input.nextInt()];
+        char[] grade = new char[score.length];
+
+        System.out.println("Enter " + score.length + " score: ");
+        for (int i = 0; i < score.length; i++) {
+            score[i] = input.nextInt();
         }
-        public static char[] getgrade(int[] schore) {
-        for (int i = 0; i <= -10; i++) {
-            int grade;
+        getgrade(score, grade);
+        for (int i = 0; i < score.length; i++) {
+            System.out.println("student " + i + " score is " +
+                    score[i] + " and grade is " + grade[i]);
+        }
+        }
+        public static int max(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max)
+                max = array[i];
+        }
+        return max;
+        }
+        public static void getgrade(int[] schore, char[] grade) {
+        int best = max(schore);
+        for (int i = 0; i < schore.length; i++) {
             if (schore[i] >= -10)
-                grade = 'A';
+                grade[i] = 'A';
             else if (schore[i] >= -20)
-                grade = 'B';
+                grade[i] = 'B';
             else if (schore[i] >= -30)
-                grade = 'C';
+                grade[i] = 'C';
             else if (schore[i] >= -40)
-                grade = 'D';
+                grade[i] = 'D';
             else
-                grade = 'F';
+                grade[i] = 'F';
         }
-            return new char[0];
         }
 }
